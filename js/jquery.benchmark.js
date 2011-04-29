9df2a68feba6daa5f509e7ceac9258f6a43cb3e3
Mike Samuel <mikesamuel@gmail.com>
2011-04-29 22:35:30 +0000

    Fixed benchmarks
    git-svn-id: https://js-quasis-libraries-and-repl.googlecode.com/svn/trunk@107 4767c97d-3c7d-a95c-628f-469c0298ba8a

--- js/jquery.benchmark.js
+++ js/jquery.benchmark.js
@@ -1,11 +1,23 @@
-var output = $("#output").empty();
+var queue = [];
 
-var nRunning = 0;
+$.benchmarkRunning = false;
 
-// based on methodology developed by PPK:
-// http://www.quirksmode.org/blog/archives/2009/08/when_to_read_ou.html
-$.benchmark = function(times, result_selector, func){
-  ++nRunning;
+$.runBenchmark = function () {
+  if (queue.length === 0) {
+    $.benchmarkRunning = false;
+    try {
+      // Call out to template-benchmarks.html to run next.
+      window.parent.signalDone();
+    } catch (ex) {
+      // ok
+    }
+    return; 
+  }
+
+  var task = queue[0];
+  queue.splice(0, 1);
+
+  var times = task[0], result_selector = task[1], func = task[2];
 
   var i = times;
   var startTime = +new Date;
@@ -14,14 +26,24 @@ $.benchmark = function(times, result_selector, func){
   // Intentionally delay sampling endTime until after DOM has been rendered.
   setTimeout(function () {
     var endTime = +new Date;
+    if (times !== 1) { output.empty(); }
     var result = (endTime-startTime)/times;
-    $(result_selector).text(result.toFixed(3));
-    output.empty();
-    if (!--nRunning) {
-      // Call out to template-benchmarks.html to run next.
-      window.parent.signalDone();
-    }
+    $(result_selector).append(result.toFixed(3) + "/run for " + times + ". ");
+
+    setTimeout($.runBenchmark, 100);
   },10);
+};
+
+// based on methodology developed by PPK:
+// http://www.quirksmode.org/blog/archives/2009/08/when_to_read_ou.html
+$.benchmark = function(times, result_selector, func){
+  var kickoff = queue.length == 0;
+
+  queue.push([times, result_selector, func]);
+  if (!$.benchmarkRunning) {
+    $.benchmarkRunning = true;
+    $.runBenchmark();
+  }
 };
 
 $.benchmarks = {};
